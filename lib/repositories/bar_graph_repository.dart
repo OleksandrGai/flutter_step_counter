19024d47f201a692bae2f588ab6ea22cceddcff6
OleksandrGailiunas <alexgailiunas0011@gmail.com>
2023-10-14 16:23:16 +0300

    remake Bar Graph

--- lib/repositories/bar_graph_repository.dart
+++ lib/repositories/bar_graph_repository.dart
@@ -1,41 +1,144 @@
 class GraphCoordinateSystem {
   GraphCoordinateSystem({required this.x, required this.y});
 
-  final int x;
+  final DateTime x;
   final int y;
 }
 
 class BarData {
   BarData({
-    required this.first,
-    required this.second,
-    required this.third,
-    required this.fourth,
-    required this.firstStepCount,
-    required this.secondStepCount,
-    required this.thirdStepCount,
-    required this.fourthStepCount,
+    required this.oneAm,
+    required this.twoAm,
+    required this.threeAm,
+    required this.fourAm,
+    required this.fiveAm,
+    required this.sixAm,
+    required this.sevenAm,
+    required this.eightAm,
+    required this.nineAm,
+    required this.tenAm,
+    required this.elevenAm,
+    required this.midday,
+    required this.onePm,
+    required this.twoPm,
+    required this.threePm,
+    required this.fourPm,
+    required this.fivePm,
+    required this.sixPm,
+    required this.sevenPm,
+    required this.eightPm,
+    required this.ninePm,
+    required this.tenPm,
+    required this.elevenPm,
+    required this.midNight,
 
+    // sepCount
+    required this.oneAmStepCount,
+    required this.twoAmStepCount,
+    required this.threeAmStepCount,
+    required this.fourAmStepCount,
+    required this.fiveAmStepCount,
+    required this.sixAmStepCount,
+    required this.sevenAmStepCount,
+    required this.eightAmStepCount,
+    required this.nineAmStepCount,
+    required this.tenAmStepCount,
+    required this.elevenAmStepCount,
+    required this.middayStepCount,
+    required this.onePmStepCount,
+    required this.twoPmStepCount,
+    required this.threePmStepCount,
+    required this.fourPmStepCount,
+    required this.fivePmStepCount,
+    required this.sixPmStepCount,
+    required this.sevenPmStepCount,
+    required this.eightPmStepCount,
+    required this.ninePmStepCount,
+    required this.tenPmStepCount,
+    required this.elevenPmStepCount,
+    required this.midNightStepCount,
   });
 
-  final int first;
-  final int second;
-  final int third;
-  final int fourth;
+  final DateTime oneAm;
+  final DateTime twoAm;
+  final DateTime threeAm;
+  final DateTime fourAm;
+  final DateTime fiveAm;
+  final DateTime sixAm;
+  final DateTime sevenAm;
+  final DateTime eightAm;
+  final DateTime nineAm;
+  final DateTime tenAm;
+  final DateTime elevenAm;
+  final DateTime midday;
 
-  final int firstStepCount;
-  final int secondStepCount;
-  final int thirdStepCount;
-  final int fourthStepCount;
+  final DateTime onePm;
+  final DateTime twoPm;
+  final DateTime threePm;
+  final DateTime fourPm;
+  final DateTime fivePm;
+  final DateTime sixPm;
+  final DateTime sevenPm;
+  final DateTime eightPm;
+  final DateTime ninePm;
+  final DateTime tenPm;
+  final DateTime elevenPm;
+  final DateTime midNight;
+
+  // sepCount
+  final int oneAmStepCount;
+  final int twoAmStepCount;
+  final int threeAmStepCount;
+  final int fourAmStepCount;
+  final int fiveAmStepCount;
+  final int sixAmStepCount;
+  final int sevenAmStepCount;
+  final int eightAmStepCount;
+  final int nineAmStepCount;
+  final int tenAmStepCount;
+  final int elevenAmStepCount;
+  final int middayStepCount;
+  final int onePmStepCount;
+  final int twoPmStepCount;
+  final int threePmStepCount;
+  final int fourPmStepCount;
+  final int fivePmStepCount;
+  final int sixPmStepCount;
+  final int sevenPmStepCount;
+  final int eightPmStepCount;
+  final int ninePmStepCount;
+  final int tenPmStepCount;
+  final int elevenPmStepCount;
+  final int midNightStepCount;
 
   List<GraphCoordinateSystem> barData = [];
 
   void init() {
     barData = [
-      GraphCoordinateSystem(x: first, y: firstStepCount),
-      GraphCoordinateSystem(x: second, y: secondStepCount),
-      GraphCoordinateSystem(x: third, y: thirdStepCount),
-      GraphCoordinateSystem(x: fourth, y: fourthStepCount),
+      GraphCoordinateSystem(x: oneAm, y: oneAmStepCount),
+      GraphCoordinateSystem(x: twoAm, y: twoAmStepCount),
+      GraphCoordinateSystem(x: threeAm, y: threeAmStepCount),
+      GraphCoordinateSystem(x: fourAm, y: fourAmStepCount),
+      GraphCoordinateSystem(x: fiveAm, y: fiveAmStepCount),
+      GraphCoordinateSystem(x: sixAm, y: sixAmStepCount),
+      GraphCoordinateSystem(x: sevenAm, y: sevenAmStepCount),
+      GraphCoordinateSystem(x: eightAm, y: eightAmStepCount),
+      GraphCoordinateSystem(x: nineAm, y: nineAmStepCount),
+      GraphCoordinateSystem(x: tenAm, y: tenAmStepCount),
+      GraphCoordinateSystem(x: elevenAm, y: elevenAmStepCount),
+      GraphCoordinateSystem(x: midday, y: middayStepCount),
+      GraphCoordinateSystem(x: onePm, y: onePmStepCount),
+      GraphCoordinateSystem(x: twoPm, y: twoPmStepCount),
+      GraphCoordinateSystem(x: threePm, y: threePmStepCount),
+      GraphCoordinateSystem(x: fourPm, y: fourPmStepCount),
+      GraphCoordinateSystem(x: fivePm, y: fivePmStepCount),
+      GraphCoordinateSystem(x: sixPm, y: sixPmStepCount),
+      GraphCoordinateSystem(x: sevenPm, y: sevenPmStepCount),
+      GraphCoordinateSystem(x: eightPm, y: eightPmStepCount),
+      GraphCoordinateSystem(x: ninePm, y: ninePmStepCount),
+      GraphCoordinateSystem(x: tenPm, y: tenPmStepCount),
+      GraphCoordinateSystem(x: elevenPm, y: elevenPmStepCount),
+      GraphCoordinateSystem(x: midNight, y: midNightStepCount),
     ];
   }
 }
--- lib/screens/testScreens/homeScreen.dart
+++ lib/screens/testScreens/homeScreen.dart
@@ -17,11 +17,33 @@ class HomeScreen extends StatefulWidget {
 
 class _SplashScreenState2 extends State<HomeScreen> {
   final now = DateTime.now();
+  final tomorrow = DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day + 1);
   HealthFactory healthFactory = HealthFactory();
   int _firstSteps = 0;
   int _secondSteps = 0;
   int _thirdSteps = 0;
   int _fourthSteps = 0;
+  int _fiveSteps = 0;
+  int _sixSteps = 0;
+  int _sevenSteps = 0;
+  int _eightSteps = 0;
+  int _nineSteps = 0;
+  int _tenSteps = 0;
+  int _elevenSteps = 0;
+  int _middaySteps = 0;
+  int _firstPmSteps = 0;
+  int _secondPmSteps = 0;
+  int _thirdPmSteps = 0;
+  int _fourthPmSteps = 0;
+  int _fivePmSteps = 0;
+  int _sixPmSteps = 0;
+  int _sevenPmSteps = 0;
+  int _eightPmSteps = 0;
+  int _ninePmSteps = 0;
+  int _tenPmSteps = 0;
+  int _elevenPmSteps = 0;
+  int _midNightSteps = 0;
 
   @override
   void initState() {
@@ -37,9 +59,79 @@ class _SplashScreenState2 extends State<HomeScreen> {
           now.year,
           now.month,
           now.day,
+          const TimeOfDay(hour: 00, minute: 59).hour,
+          const TimeOfDay(hour: 00, minute: 59).minute),
+    ).then((value) => _firstSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 01, minute: 00).hour,
+          const TimeOfDay(hour: 01, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 01, minute: 59).hour,
+          const TimeOfDay(hour: 01, minute: 59).minute),
+    ).then((value) => _secondSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 02, minute: 00).hour,
+          const TimeOfDay(hour: 02, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 02, minute: 59).hour,
+          const TimeOfDay(hour: 02, minute: 59).minute),
+    ).then((value) => _thirdSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 03, minute: 00).hour,
+          const TimeOfDay(hour: 03, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 03, minute: 59).hour,
+          const TimeOfDay(hour: 03, minute: 59).minute),
+    ).then((value) => _fourthSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 04, minute: 00).hour,
+          const TimeOfDay(hour: 04, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 04, minute: 59).hour,
+          const TimeOfDay(hour: 04, minute: 59).minute),
+    ).then((value) => _fiveSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 05, minute: 00).hour,
+          const TimeOfDay(hour: 05, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
           const TimeOfDay(hour: 05, minute: 59).hour,
           const TimeOfDay(hour: 05, minute: 59).minute),
-    ).then((value) => _firstSteps = value);
+    ).then((value) => _sixSteps = value);
     fetchStepData(
       startTime: DateTime(
           now.year,
@@ -51,9 +143,79 @@ class _SplashScreenState2 extends State<HomeScreen> {
           now.year,
           now.month,
           now.day,
+          const TimeOfDay(hour: 06, minute: 59).hour,
+          const TimeOfDay(hour: 06, minute: 59).minute),
+    ).then((value) => _sevenSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 07, minute: 00).hour,
+          const TimeOfDay(hour: 07, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 07, minute: 59).hour,
+          const TimeOfDay(hour: 07, minute: 59).minute),
+    ).then((value) => _eightSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 08, minute: 00).hour,
+          const TimeOfDay(hour: 08, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 08, minute: 59).hour,
+          const TimeOfDay(hour: 08, minute: 59).minute),
+    ).then((value) => _nineSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 09, minute: 00).hour,
+          const TimeOfDay(hour: 09, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 09, minute: 59).hour,
+          const TimeOfDay(hour: 09, minute: 59).minute),
+    ).then((value) => _tenSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 10, minute: 00).hour,
+          const TimeOfDay(hour: 10, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 10, minute: 59).hour,
+          const TimeOfDay(hour: 10, minute: 59).minute),
+    ).then((value) => _elevenSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 11, minute: 00).hour,
+          const TimeOfDay(hour: 11, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
           const TimeOfDay(hour: 11, minute: 59).hour,
           const TimeOfDay(hour: 11, minute: 59).minute),
-    ).then((value) => _secondSteps = value);
+    ).then((value) => _middaySteps = value);
     fetchStepData(
       startTime: DateTime(
           now.year,
@@ -65,9 +227,79 @@ class _SplashScreenState2 extends State<HomeScreen> {
           now.year,
           now.month,
           now.day,
+          const TimeOfDay(hour: 12, minute: 59).hour,
+          const TimeOfDay(hour: 12, minute: 59).minute),
+    ).then((value) => _firstPmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 13, minute: 00).hour,
+          const TimeOfDay(hour: 13, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 13, minute: 59).hour,
+          const TimeOfDay(hour: 13, minute: 59).minute),
+    ).then((value) => _secondPmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 14, minute: 00).hour,
+          const TimeOfDay(hour: 14, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 14, minute: 59).hour,
+          const TimeOfDay(hour: 14, minute: 59).minute),
+    ).then((value) => _thirdPmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 15, minute: 00).hour,
+          const TimeOfDay(hour: 15, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 15, minute: 59).hour,
+          const TimeOfDay(hour: 15, minute: 59).minute),
+    ).then((value) => _fourthPmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 16, minute: 00).hour,
+          const TimeOfDay(hour: 16, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 16, minute: 59).hour,
+          const TimeOfDay(hour: 16, minute: 59).minute),
+    ).then((value) => _fivePmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 17, minute: 00).hour,
+          const TimeOfDay(hour: 17, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
           const TimeOfDay(hour: 17, minute: 59).hour,
           const TimeOfDay(hour: 17, minute: 59).minute),
-    ).then((value) => _thirdSteps = value);
+    ).then((value) => _sixPmSteps = value);
     fetchStepData(
       startTime: DateTime(
           now.year,
@@ -79,9 +311,79 @@ class _SplashScreenState2 extends State<HomeScreen> {
           now.year,
           now.month,
           now.day,
-          const TimeOfDay(hour: 23, minute: 59).hour,
-          const TimeOfDay(hour: 23, minute: 59).minute),
-    ).then((value) => _fourthSteps = value);
+          const TimeOfDay(hour: 18, minute: 59).hour,
+          const TimeOfDay(hour: 18, minute: 59).minute),
+    ).then((value) => _sevenPmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 19, minute: 00).hour,
+          const TimeOfDay(hour: 19, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 19, minute: 59).hour,
+          const TimeOfDay(hour: 19, minute: 59).minute),
+    ).then((value) => _eightPmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 20, minute: 00).hour,
+          const TimeOfDay(hour: 20, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 20, minute: 59).hour,
+          const TimeOfDay(hour: 20, minute: 59).minute),
+    ).then((value) => _ninePmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 21, minute: 00).hour,
+          const TimeOfDay(hour: 21, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 21, minute: 59).hour,
+          const TimeOfDay(hour: 21, minute: 59).minute),
+    ).then((value) => _tenPmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 22, minute: 00).hour,
+          const TimeOfDay(hour: 22, minute: 00).minute),
+      endTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 22, minute: 59).hour,
+          const TimeOfDay(hour: 22, minute: 59).minute),
+    ).then((value) => _elevenPmSteps = value);
+    fetchStepData(
+      startTime: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          const TimeOfDay(hour: 23, minute: 00).hour,
+          const TimeOfDay(hour: 23, minute: 00).minute),
+      endTime: DateTime(
+          tomorrow.year,
+          tomorrow.month,
+          tomorrow.day,
+          const TimeOfDay(hour: 00, minute: 00).hour,
+          const TimeOfDay(hour: 00, minute: 00).minute),
+    ).then((value) => _midNightSteps = value);
   }
 
   Future<int> fetchStepData({
@@ -100,7 +402,7 @@ class _SplashScreenState2 extends State<HomeScreen> {
         print(e);
       }
       print('Total number os steps: $step');
-      return step!;
+      return step ?? 0;
     } else {
       print('error some');
       return 0;
@@ -110,40 +412,71 @@ class _SplashScreenState2 extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocListener<LogoutBloc, LogoutState>(
-        listener: (context, state) {
-          if (state is LogoutSucceed) {
-            Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (_) => const LoginPage()));
-          }
-        },
-        child: BlocBuilder<LogoutBloc, LogoutState>(builder: (context, state) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                    height: 400,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text('Today: ${now.year}-${now.month}-${now.day}', style: TextStyle(fontSize: 20, color: Colors.black),),
+      ),
+      body: SafeArea(
+        child: BlocListener<LogoutBloc, LogoutState>(
+          listener: (context, state) {
+            if (state is LogoutSucceed) {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (_) => const LoginPage()));
+            }
+          },
+          child:
+              BlocBuilder<LogoutBloc, LogoutState>(builder: (context, state) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 5, right: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Flexible(
                     child: BarGraph(
-                      firstStepCount: _firstSteps,
-                      secondStepCount: _secondSteps,
-                      thirdStepCount: _thirdSteps,
-                      fourthStepCount: _fourthSteps,
-                    )),
-                Text(
-                  (_firstSteps + _secondSteps + _thirdSteps + _fourthSteps)
-                      .toString(),
-                  style: TextStyle(fontSize: 45),
-                ),
-                TextButton(
-                    onPressed: () {
-                      context.read<LogoutBloc>().add(LogoutButtonPressed());
-                    },
-                    child: Text('Log out'))
-              ],
-            ),
-          );
-        }),
+                      oneAmStepCount: _firstSteps,
+                      twoAmStepCount: _secondSteps,
+                      threeAmStepCount: _thirdSteps,
+                      fourAmStepCount: _fourthSteps,
+                      fiveAmStepCount: _fiveSteps,
+                      sixAmStepCount: _sixSteps,
+                      sevenAmStepCount: _sevenSteps,
+                      eightAmStepCount: _eightSteps,
+                      nineAmStepCount: _nineSteps,
+                      tenAmStepCount: _tenSteps,
+                      elevenAmStepCount: _elevenSteps,
+                      middayStepCount: _middaySteps,
+                      // PM
+                      onePmStepCount: _firstPmSteps,
+                      twoPmStepCount: _secondPmSteps,
+                      threePmStepCount: _thirdPmSteps,
+                      fourPmStepCount: _fourthPmSteps,
+                      fivePmStepCount: _fivePmSteps,
+                      sixPmStepCount: _sixPmSteps,
+                      sevenPmStepCount: _sevenPmSteps,
+                      eightPmStepCount: _eightPmSteps,
+                      ninePmStepCount: _ninePmSteps,
+                      tenPmStepCount: _tenPmSteps,
+                      elevenPmStepCount: _elevenPmSteps,
+                      midNightStepCount: _midNightSteps,
+                    ),
+                  ),
+                  Text(
+                    ('Total ${_firstSteps + _secondSteps + _thirdSteps + _fourthSteps + _fiveSteps + _sixSteps + _sevenSteps + _eightSteps + _nineSteps + _tenSteps + _elevenSteps + _midNightSteps} steps')
+                        .toString(),
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        context.read<LogoutBloc>().add(LogoutButtonPressed());
+                      },
+                      child: Text('Log out'))
+                ],
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
